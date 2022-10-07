251368bfc286bdf753688437e655d08a134e05f1
BubusaraQA <114005583+BubusaraQA@users.noreply.github.com>
2022-10-07 14:25:00 -0400

    Initial commit

---
+++ HomeAssignmemt.java
@@ -0,0 +1,46 @@
+package module2;
+
+public class HomeAssignmemt {
+    public static void main(String[] args) {
+        String txt1 = "Hello";
+        String txt2 = "Sara Erkinova";
+
+
+        int num1 = -5;
+        char num2 = 8;
+        char num3 = 6;
+        char num4 = 20;
+        int num5 = -3;
+        char num7 = 5;
+
+        int result = num1 + num2 * num3;
+        double result2 = num4 + (double) num5 * num7 / num2;
+
+
+        double a = 1.1;
+        double b = 2.2;
+        double c = 3.333;
+        double d = 4.44;
+        double f = 5.5;
+
+        double result3 = a * b * c * d * f;
+
+        System.out.println(txt1);
+        System.out.println(txt2);
+
+        System.out.println("a. -5 + 8 * 6 = " + result);
+        System.out.println("b. 20 + ( - 3) * 5 / 8 = " + result2);
+
+        System.out.println(a + " * " + b + " * " + c + " * " + d + " * " + f + " = " + result3);
+
+
+
+
+
+
+
+
+
+
+    }
+}
